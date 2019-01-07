5472fe6601cd819cd9e2d159fef8cc2036e767db
kentotakeuchi <ideal.kt.reality@gmail.com>
2019-01-06 17:12:47 -0800

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Header.css';
+
+const header = () => (
+    <div className={classes.Header}>
+        <p>kento takeuchi</p>
+        <p>web developer / santa clara</p>
+    </div>
+);
+
+export default header;
