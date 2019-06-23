4fcfa055b3d41f83cf3883f99e7c17b988c78e0e
kentotakeuchi <ideal.kt.reality@gmail.com>
2019-06-23 13:21:41 -0700

    updated info for bank app

--- src/components/NewsComponent/NewsComponent.js
+++ src/components/NewsComponent/NewsComponent.js
@@ -20,7 +20,10 @@ const NewsComponent = (props) => {
                 <p>post on 2019.06.17 15:10:33</p>
                 <img src={bank} alt="bank" />
                 <article>
-                BANK is an application like flash card to consolidate vocabulary in English. Users can store phrases users wanted to say and review them at random whenever users have time.
+                BANK is an application like flash card to consolidate your knowleadge you would like to memorize. you are able to store anything you would like to learn and review them at random anytime, anywhere.<br/>
+                <br/>
+                Tips: If you are an iOS user, it's good to have iOS home screen icon for this app. Since the icon has been prepared, you are able to use the app like iOS app.<br/>
+                Open Safari browser > Click Action(Share) icon > Click Add to Home Screen > Save
                 </article>
                 <a href="https://bank-of-something.herokuapp.com/">&diams; go to bank</a>
 
@@ -61,7 +64,10 @@ const NewsComponent = (props) => {
                 <p>post on 2019.06.17 15:10:33</p>
                 <img src={bank} alt="bank" />
                 <article>
-                BANK is an application like flash card to consolidate your knowleadge you would like to memorize. you are able to store anything you would like to learn and review them at random anytime, anywhere.
+                BANK is an application like flash card to consolidate your knowleadge you would like to memorize. you are able to store anything you would like to learn and review them at random anytime, anywhere.<br/>
+                <br/>
+                Tips: If you are an iOS user, it's good to have iOS home screen icon for this app. Since the icon has been prepared, you are able to use the app like iOS app.<br/>
+                Open Safari browser > Click Action(Share) icon > Click Add to Home Screen > Save
                 </article>
                 <a href="https://bank-of-something.herokuapp.com/">&diams; go to bank</a>
 
